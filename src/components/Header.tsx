a41b99923f3260946a102bdaede04abbb05bbf7d
San-Tri <stri80603@gmail.com>
2026-06-24 17:54:53 -0500

    feat: agregar contenido al Header e integrar iconos de Lucide

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,37 @@
-function Header(){
-    return <p>header</p>
+import { Leaf, Menu } from "lucide-react";
+
+function Header() {
+  return (
+    <>
+      <header>
+        <div className="logo">
+          <Leaf />
+          <p>DXN Bienestar</p>
+        </div>
+        <div className="accesos_directos">
+          <nav>
+            <ul>
+              <li>
+                <a href="#">Inicio</a>
+              </li>
+              <li>
+                <a href="#">Productos</a>
+              </li>
+              <li>
+                <a href="#">Novedades</a>
+              </li>
+              <li>
+                <a href="#">Afiliate</a>
+              </li>
+              <li>
+                <a href="#">Contacto</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+    </>
+  );
 }
 
 export default Header;
