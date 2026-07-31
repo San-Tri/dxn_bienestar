fde319aa844aad0a2bcd96b90b26b8930c725174
San-Tri <stri80603@gmail.com>
2026-07-31 16:58:44 -0500

    style: agregar el Active State a los accesos directos

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -48,7 +48,7 @@ function Header() {
                 ) : (
                   <a
                     href="#"
-                    className="font-bold font-['Lexend'] text-[#10B981] underline"
+                    className="font-bold font-['Lexend'] text-[#10B981] relative after:content-[''] after:absolute after:bg-[#10B981] after:w-[210%] after:h-0.5 after:-bottom-2 after:-left-1"
                   >
                     Inicio
                   </a>
