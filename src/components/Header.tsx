5e9f19213fdea34f278446a7ed2969f830131642
San-Tri <stri80603@gmail.com>
2026-09-22 14:17:46 -0500

    feat: ajuste del diseño para vista desktop y agregar icono de de darkmode

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,80 +3,72 @@
  */
 
 import { useState } from "react";
-import { Leaf, Menu, X } from "lucide-react";
+import { Leaf, Sun, Moon } from "lucide-react";
 
 function Header() {
-  {
-    /* agregar el useState */
-  }
-  const [menuAbierto, setMenuAbierto] = useState(false);
+  /** useState para manejar el modo oscuro y claro */
+  const [darkMode, setDarkMode] = useState(false);
+  /** useState para marcar el acceso directo seleccionado */
   const [seccionMarcado, setSeccionMarcado] = useState(false);
   return (
     <>
-      <header className="w-full h-auto justify-between flex items-center px-4 py-3">
-        {/* logo y nombre */}
+      <header className="w-full h-auto justify-between flex items-center px-4 py-3 border-b border-black/6">
         <div className="flex gap-2">
           <Leaf className="text-primary stroke-3" />
           <p className="font-bold text-text-primary">DXN Bienestar</p>
         </div>
 
-        {/*boton de la amburguesa */}
-        <div className="accesos_directos">
-          <button onClick={() => setMenuAbierto(!menuAbierto)}>
-            {/*si es falso muestra el menu y si es verdadero muestra la X */}
-            {menuAbierto ? (
-              <X className="text-text-primary" />
+        {/* Uso del hidden para ocultar el menú de navegación en pantallas pequeñas*/}
+        <nav className={"hidden md:flex"}>
+          <ul className="flex gap-10">
+            <li onClick={() => setSeccionMarcado(!seccionMarcado)} className="">
+              {seccionMarcado ? (
+                <a href="#" className="text-text-primary">
+                  Inicio
+                </a>
+              ) : (
+                // uso el after para crear el Active State del enlace directo
+                <a
+                  href="#"
+                  className="text-primary relative after:content-[''] after:absolute after:bg-primary after:w-[200%] after:h-0.5 after:-bottom-2 after:-left-5"
+                >
+                  Inicio
+                </a>
+              )}
+            </li>
+            <li className="">
+              <a href="#" className="text-text-primary">
+                Productos
+              </a>
+            </li>
+            <li className="">
+              <a href="#" className="text-text-primary">
+                Novedades
+              </a>
+            </li>
+            <li className="">
+              <a href="#" className="text-text-primary">
+                Contacto
+              </a>
+            </li>
+          </ul>
+        </nav>
+
+        <div className="flex gap-4">
+          <button className="hidden md:block rounded-full px-5 py-2 font-bold bg-accent">
+            Afíliate
+          </button>
+          <button
+            onClick={() => setDarkMode(!darkMode)}
+            className="border border-[#A7F3D0]/50 bg-[#ECFDF5] rounded-full p-2"
+          >
+            {darkMode ? (
+              <Moon className="text-text-primary" />
             ) : (
-              <Menu className="text-text-primary" />
+              <Sun className="text-text-primary" />
             )}
           </button>
         </div>
-
-        {/*barra de navegacion */}
-        {menuAbierto && (
-          <nav className="absolute top-13.5 left-0 backdrop-blur-lg w-full z-50">
-            <ul className="ml-20">
-              <li
-                onClick={() => setSeccionMarcado(!seccionMarcado)}
-                className="py-5"
-              >
-                {seccionMarcado ? (
-                  <a href="#" className="font-bold text-white">
-                    Inicio
-                  </a>
-                ) : (
-                  // usamos el after para crear el Active State del enlace directo
-                  <a
-                    href="#"
-                    className="font-bold text-primary relative after:content-[''] after:absolute after:bg-primary after:w-[210%] after:h-0.5 after:-bottom-2 after:-left-1"
-                  >
-                    Inicio
-                  </a>
-                )}
-              </li>
-              <li className="py-5">
-                <a href="#" className="font-bold text-white">
-                  Productos
-                </a>
-              </li>
-              <li className="py-5">
-                <a href="#" className="font-bold text-white">
-                  Novedades
-                </a>
-              </li>
-              <li className="py-5">
-                <a href="#" className="font-bold text-white">
-                  Afiliate
-                </a>
-              </li>
-              <li className="py-5">
-                <a href="#" className="font-bold text-white">
-                  Contacto
-                </a>
-              </li>
-            </ul>
-          </nav>
-        )}
       </header>
     </>
   );
